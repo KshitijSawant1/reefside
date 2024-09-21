03f22fb763cd1d263b544ccfaa5b70f6a59662b0
maaz <maazshaikh2079@gmail.com>
2024-09-21 22:05:34 +0530

    firebase-config

--- src/App.js
+++ src/App.js
@@ -6,13 +6,46 @@ import Admin from "./components/Admin";
 import Title from "./components/Title";
 import Paymentlog from "./components/Paymentlog";
 import Table from "./components/Table";
+//New->
+import { db } from "./components/firebase-config.js";
+
+
+//New->
+import { 
+  collection, 
+  getDocs,   // R
+  addDoc,    // C
+  updateDoc, // U
+  doc,       // U
+  deleteDoc  // D
+} from "firebase/firestore";
+
 
 function MainPage() {
   const [currentdish, setCurrentdish] = useState([]);
 
-  const clearReceipt = () => {
+  //New->
+  const usersCollectionRef = collection(db, "Orders");
+
+  const clearReceipt = async () => {
+
+    console.log(currentdish);
+    await addDoc(usersCollectionRef, { currentdish });
     setCurrentdish([]);
-  };
+
+};
+
+  //New->
+  // const [newName, setNewName] = useState("");
+  // const [newAge, setNewAge] = useState(0);
+
+  // const createUser = async () => {
+  //   await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  // }
+
+  // <button onClick={createUser}>Create User</button><br/><br/>
+
+
 
   return (
     <div className="App">
--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -1,11 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCM2mAhRVPQNlSyNwdh-z8asUK7aq1x6lY",
   authDomain: "reefside-19313.firebaseapp.com",
