7a85d3d9b7dcd888c90c0828189c317b73b8da28
maaz <maazshaikh2079@gmail.com>
2024-09-29 09:56:40 +0530

    added reading & deleting of data from database

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,11 +1,56 @@
-// src/components/Admin.js
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { db } from "./firebase-config.js";
+import { 
+  collection, 
+  getDocs,  // R
+  doc,      // R & U
+  deleteDoc // D
+} from "firebase/firestore";
 
 const Admin = () => {
+  const [orders, setOrders] = useState([]);
+  const ordersCollectionRef = collection(db, "Orders");
+
+  useEffect(() => {
+    const getOrders = async () => {
+      const data = await getDocs(ordersCollectionRef);
+      setOrders(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getOrders();
+  }, []);
+
+  const deleteOrder = async (id) => {
+    const orderDoc = doc(db, "Orders", id);
+    await deleteDoc(orderDoc);
+  }
+
   return (
     <div>
-      <h1>Welcome Admin</h1>
-      <p>This is the admin page where you can manage the application.</p>
+      {
+        orders.map((order) => {
+          return (
+            <div key={order.id}>
+
+              <b>Order Details:</b>
+
+              <ul type="none" >
+                {Object.keys(order.dishes).map((dishName) => (
+                  <li key={dishName}>
+                    {dishName}: {order.dishes[dishName]}
+                  </li>
+                ))}
+              </ul>
+
+              <p>GST Amount : {order.gstAmount}</p>
+              <p>Total : {order.total}</p>
+              <p>Total with GST : {order.totalWithGST}</p>
+              <button onClick={() => {deleteOrder(order.id)}}> Paid </button>
+              <br/><br/>
+
+            </div>
+          );
+        })
+      }
     </div>
   );
 };
